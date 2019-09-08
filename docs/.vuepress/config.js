250c517562e14861428f55a870f4aede5c6770c2
a820792395 <8207923@qq.com>
2019-09-08 20:36:18 +0800

    个人博客

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  title:'xiaoLi的博客',
+  description: 'Hello, welcome to xiaoLi blog',
+  base:'/',
+  dest: './dist',
+  head: [
+    ['link',{rel: 'icon', href: '/image/login.jpg'}]
+  ],
+  themeConfig:{
+    nav:[
+      {
+        text: '主页',
+        link:'/'
+      },
+      {
+        text: 'JavaScript',
+        link: '/javascript/'
+      }
+    ],
+    sidebar:{
+      '/javascript/': [
+        {
+          title: 'javascript基础',
+          collapsable: false,
+          children: [
+            'var',
+            'environment'
+          ]
+        }
+      ],
+      sidebarDepth: 0,
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true  
+heroImage: /image/login.jpg
+actionText: Let's go →
+actionLink: /javascript/
+---
+<!--默认的主题提供了一个首页（Homepage）的布局 (用于 这个网站的主页)。
+想要使用它，需要在你的根级 README.md 的 YAML front matter 指定 home: true。  -->
