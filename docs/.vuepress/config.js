18ea703e22e0937204dae494d3fc134de22facdf
彬彬无礼 <820792395@qq.com>
2021-03-02 18:24:20 +0800

    添加第八章内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -93,6 +93,7 @@ module.exports = {
             "chapter5",
             "chapter6",
             "chapter7",
+            "chapter8",
           ],
         },
       ],
--- docs/DataStructures-Algorithms/README.md
+++ docs/DataStructures-Algorithms/README.md
@@ -6,4 +6,5 @@
 [栈](./chapter4.md)  
 [队列](./chapter5.md)  
 [链表](./chapter6.md)  
-[集合](./chapter7.md)
+[集合](./chapter7.md)  
+[字典](./chapter8.md)
